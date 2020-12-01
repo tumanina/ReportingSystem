690d6c85b6bfb99951394e5863684115eeb1dcd8
Olga Tumanina <Olga@ireckonu.com>
2020-12-01 20:49:34 +0100

    Init commit

---
+++ ReportingSystem.Web/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ReportingSystem.Web.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<Controller> _logger;
+
+        public HealthController(ILogger<Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "response";
+        }
+    }
+}

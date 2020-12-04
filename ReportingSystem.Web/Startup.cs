4d6d5aed5b77e5b049ff6da227e9c21c25f03ca9
Olga Tumanina <Olga@ireckonu.com>
2020-12-04 18:45:11 +0100

    Added swagger (OpnApi) support

--- ReportingSystem.Web/Startup.cs
+++ ReportingSystem.Web/Startup.cs
@@ -6,12 +6,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using ReportingSystem.AzureStorage;
 using ReportingSystem.Logic;
 using ReportingSystem.Shared.Configuration;
 using ReportingSystem.Shared.Interfaces;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,11 +35,31 @@ namespace ReportingSystem.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+            services.AddSwaggerGenNewtonsoftSupport();
 
             services.Configure<AzureStorageConfiguration>(Configuration.GetSection(nameof(AzureStorageConfiguration)));
             services.AddScoped<IFileStorage, AzureFileStorage>();
             services.AddScoped<IFileStorageService, FileStorageService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Reporting service", Version = "v1" });
+
+                var files = Directory.GetFiles(AppContext.BaseDirectory, "*.xml");
+                foreach (var file in files)
+                {
+                    c.IncludeXmlComments(file);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +72,14 @@ namespace ReportingSystem.Web
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reporting service");
+                c.DocExpansion(DocExpansion.None);
+            });
 
+            app.UseRouting();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
